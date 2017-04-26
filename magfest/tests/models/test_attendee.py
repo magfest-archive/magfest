e6ea516bb33ecaa227f8257314d1e5ff4a5bd8dc
Victoria Earl <kitsuta@gmail.com>
2017-04-26 13:20:31 -0400

    Fix unit tests

--- magfest/tests/models/test_attendee.py
+++ magfest/tests/models/test_attendee.py
@@ -1,5 +1,6 @@
 from uber import config
 from uber.tests import *
+from uber.tests.conftest import *
 
 
 class TestCosts:
@@ -8,14 +9,16 @@ class TestCosts:
         monkeypatch.setattr(c, 'get_oneday_price', Mock(return_value=10))
         monkeypatch.setattr(c, 'get_attendee_price', Mock(return_value=40))
 
-    def test_half_price_discount(self, monkeypatch):
+    def test_half_price_discount(self):
         # Age group discount not set: badge is half off
         assert 20 == Attendee(age_group=c.UNDER_13).badge_cost
 
+    def test_half_price_overrides_age_discount(self, monkeypatch):
         # Age group discount is less than half off: badge is half off
-        monkeypatch.setattr(Attendee, 'age_group_conf', {'discount': 5})
+        monkeypatch.setattr(Attendee, 'age_group_conf', {'val': c.UNDER_13, 'discount': 5})
         assert 20 == Attendee(age_group=c.UNDER_13).badge_cost
 
+    def test_age_discount_overrides_half_price(self, monkeypatch):
         # Age group discount is greater than half off: badge price based on age discount instead
         monkeypatch.setattr(Attendee, 'age_group_conf', {'discount': 30})
         assert 10 == Attendee(age_group=c.UNDER_13).badge_cost
